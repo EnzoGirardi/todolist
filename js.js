7fe77be735dc1ba7f2be12e163338e566e53d238
LuizBlume <luiz.fernando.251107@gmail.com>
2023-12-05 19:34:35 -0300

    foco e enter adicionadas

--- js.js
+++ js.js
@@ -5,29 +5,36 @@ let botaolimpar = document.getElementById("limpar");
 let botaolimpar2 = document.getElementById("limpar2")
 let tarefacompleta = document.getElementById("containertarefacompleta")
 
-botaoadiconar.addEventListener("click", function(test){ //No botaoadicionar, quando ele ouvir o "click", irá criar a function
-    if(campoinput.value.trim() !== ""){
 
-    let paragrafo = document.createElement("p"); //criei uma let que irá criar um elemento, no caso <p>
-    paragrafo.classList.add('paragrafoestilo'); //o classlist funciona para adicionar css ao <p> acima
-    paragrafo.innerText = campoinput.value; //o innertext server para adicionar o nome da tarefa que está escrito no campo para o <p> criado
-    containertarefa.appendChild(paragrafo); //o campo containertarefa será onde o <p> será acrescentado com o AppendChild
-    campoinput.value = "" //após todo uso o valor do campoinput será reiniciado para nada
-    paragrafo.addEventListener("click", function(){ //no paragrafo, quando ele ouvir o "click", irá criar a function
-        containertarefa.removeChild(paragrafo); //quando você clicar no <p>(paragrafo), ele será removido do containertarefa e será anexado no tarefacompleta, outra lista abaixo
-        tarefacompleta.appendChild(paragrafo);
-    })
-    botaolimpar.addEventListener("click", function(){ //No botaolimpar, quando ele ouvir o "click", irá criar a function
-        containertarefa.removeChild(paragrafo); //quando você clicar no buttonlimpar, ele será removido do containertarefa e será anexado no tarefacompleta, outra lista abaixo
-        tarefacompleta.appendChild(paragrafo);
-    })
-    botaolimpar2.addEventListener("click", function(){
-        tarefacompleta.removeChild(paragrafo); //Adicionei um último button na lista de tarefas completas que, após o "click", todas as tarefas prontas serão apagadas
-    })
-    
-}})
-document.addEventListener("keypress", function(event){
-    if(event.keyCode === 13){
-        test();
+function adicionarTarefa() { // Função para adicionar uma tarefa
+    if (campoinput.value.trim() !== "") { // Verifica se o campo de input não está vazio
+        let paragrafo = document.createElement("p"); // Cria um parágrafo (<p>) para a nova tarefa
+        paragrafo.classList.add('paragrafoestilo'); // Adiciona uma classe ao parágrafo para aplicar estilos (se necessário)
+        paragrafo.innerText = campoinput.value; // Define o texto do parágrafo com o valor do campo de input
+        containertarefa.appendChild(paragrafo); // Adiciona o parágrafo ao container de tarefas pendentes
+        campoinput.value = ""; // Limpa o valor do campo de input
+        campoinput.focus(); // Define o foco de volta ao campo de input
+        paragrafo.addEventListener("click", function () { // Adiciona um ouvinte de evento para o clique no parágrafo
+            containertarefa.removeChild(paragrafo);// Remove o parágrafo do container de tarefas pendentes
+            tarefacompleta.appendChild(paragrafo);// Adiciona o parágrafo ao container de tarefas completas
+            campoinput.focus(); // Define o foco de volta ao campo de input
+        });
+        botaolimpar.addEventListener("click", function () { // Adiciona um ouvinte de evento para o clique no botão de limpar
+            containertarefa.removeChild(paragrafo); // Remove o parágrafo do container de tarefas pendentes
+            tarefacompleta.appendChild(paragrafo); // Adiciona o parágrafo ao container de tarefas completas
+            campoinput.focus(); // Define o foco de volta ao campo de input
+        });
+        botaolimpar2.addEventListener("click", function () { // Adiciona um ouvinte de evento para o clique no segundo botão de limpar 
+            tarefacompleta.removeChild(paragrafo);// Remove o parágrafo do container de tarefas completas
+            campoinput.focus(); // Define o foco de volta ao campo de input
+        });
     }
-})
+}
+// Adiciona um ouvinte de evento para o clique no botão de adicionar
+botaoadiconar.addEventListener("click", adicionarTarefa);
+// Adiciona um ouvinte de evento para a tecla Enter
+document.addEventListener("keypress", function (event) { // Verifica se a tecla pressionada é Enter (código 13)
+    if (event.keyCode === 13) { // Chama a função para adicionar uma tarefa
+        adicionarTarefa();
+    }
+});
