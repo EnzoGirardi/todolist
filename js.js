5ff97dff1fc4645edd94f56170e736f401aec5c9
EnzoGirardi <enzogirardi1903@gmail.com>
2023-12-05 01:25:40 -0300

    .

--- js.js
+++ js.js
@@ -1,7 +1,9 @@
 let botaoadiconar = document.getElementById("adicionar");
 let containertarefa = document.getElementById("containertarefa");
 let campoinput = document.getElementById("camporesultado")
-let botaolimpar = document.getElementById("limpar")
+let botaolimpar = document.getElementById("limpar");
+let botaolimpar2 = document.getElementById("limpar2")
+let tarefacompleta = document.getElementById("containertarefacompleta")
 
 botaoadiconar.addEventListener("click", function(){
     let paragrafo = document.createElement("p");
@@ -11,12 +13,14 @@ botaoadiconar.addEventListener("click", function(){
     campoinput.value = ""
     paragrafo.addEventListener("click", function(){
         containertarefa.removeChild(paragrafo);
-
+        tarefacompleta.appendChild(paragrafo);
     })
     botaolimpar.addEventListener("click", function(){
         containertarefa.removeChild(paragrafo);
-   
+        tarefacompleta.appendChild(paragrafo);
+    })
+    botaolimpar2.addEventListener("click", function(){
+        tarefacompleta.removeChild(paragrafo);
     })
     
 })
-
